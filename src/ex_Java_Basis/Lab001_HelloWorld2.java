9cf7cefcba74a69708fd1e3b6d60c4b973f983fe
Altaf Hussain <altafhussainmadni34@gmail.com>
2025-06-08 23:11:05 +0530

    Initial commit

---
+++ src/ex_Java_Basis/Lab001_HelloWorld2.java
@@ -0,0 +1,8 @@
+package ex_Java_Basis;
+
+public class Lab001_HelloWorld2
+{
+    public static void main(String[] args) {
+        System.out.println(2+2);
+    }
+}
---
+++ src/ex_Java_Basis/Lab001_Helloworld.java
@@ -0,0 +1,8 @@
+package ex_Java_Basis;
+
+public class Lab001_Helloworld
+{
+    public static void main(String[] args) {
+System.out.println("Hello World");
+    }
+}
