6d040df29904cdd642b91563f759d5397c6e3e0d
Altaf Hussain <altafhussainmadni34@gmail.com>
2025-06-12 23:20:10 +0530

    Comments In java

---
+++ src/ex_Java_Basis/Lab006_Comments.java
@@ -0,0 +1,10 @@
+package ex_Java_Basis;
+
+public class Lab006_Comments {
+
+    // This is a comment, this code will nto be executed. ->Single Line comment
+         /*System.out.println("Hi");
+         System.out.println("Bye");
+         System.out.println("Drink");
+         System.out.println("Chai");*/ /* Block or multiline comment */
+}
