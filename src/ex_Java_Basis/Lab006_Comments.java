5fa288c645d032b91edc05fadb01c1a5d13c0933
Altaf Hussain <altafhussainmadni34@gmail.com>
2025-06-12 23:25:25 +0530

    Single Line comment

--- src/ex_Java_Basis/Lab006_Comments.java
+++ src/ex_Java_Basis/Lab006_Comments.java
@@ -2,7 +2,10 @@ package ex_Java_Basis;
 
 public class Lab006_Comments {
 
-    // This is a comment, this code will nto be executed. ->Single Line comment
+    // This is a comment, this code will not be executed.
+
+ //System.out.println("Hi"); --- Single line comment
+
          /*System.out.println("Hi");
          System.out.println("Bye");
          System.out.println("Drink");
