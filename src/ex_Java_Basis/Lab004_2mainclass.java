39d8e51d6533f75e8a7fc365ef0904041208fd19
Altaf Hussain <altafhussainmadni34@gmail.com>
2025-06-12 23:21:45 +0530

    2 main calss

---
+++ src/ex_Java_Basis/Lab004_2mainclass.java
@@ -0,0 +1,18 @@
+package ex_Java_Basis;
+
+public class Lab004_2mainclass {
+    public class Lab002_ShorcutHW {
+        public static void main(String[] args) {
+            System.out.println("Hello Test");
+        }
+
+        /*
+    public static void main(String[] args) {
+
+    }
+public static void main(int args) {
+*/
+
+    }
+    }
+
