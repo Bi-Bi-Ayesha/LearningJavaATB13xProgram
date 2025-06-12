5b8721212462fd0e2975bc00dfaf14e20ae991c0
Altaf Hussain <altafhussainmadni34@gmail.com>
2025-06-12 23:26:31 +0530

    Multi Line comment

---
+++ src/ex_Java_Basis/Lab008_MultiLineComment.java
@@ -0,0 +1,12 @@
+package ex_Java_Basis;
+
+public class Lab008_MultiLineComment {
+    public static void main(String[] args) {
+        /*
+         * Author : Pramod Dutta
+         * Course :  ATB13x
+         *
+         */
+        System.out.println("Multi Comment");
+    }
+}
