b8bd50777d502cdabb2f5cb6b48ae1465d631578
Altaf Hussain <altafhussainmadni34@gmail.com>
2025-06-14 18:06:19 +0530

    Variables1

---
+++ src/ex02_Java_Basic/Lab012_Variables.java
@@ -0,0 +1,9 @@
+package ex02_Java_Basic;
+
+public class Lab012_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 65 + 1;
+        System.out.println(a);
+    }
+}
