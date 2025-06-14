d5e34aecc0ced6bce97bec8c0038998c5b3816f5
Altaf Hussain <altafhussainmadni34@gmail.com>
2025-06-14 18:07:24 +0530

    Interview Questions

---
+++ src/ex02_Java_Basic/Lab015_InterviewQues.java
@@ -0,0 +1,11 @@
+package ex02_Java_Basic;
+
+public class Lab015_InterviewQues {
+    public static void main(String[] args) {
+        int _123 = 123;
+        System.out.println(_123);
+        int _1 = 123;
+        int _2 = 123;
+
+    }
+}
---
+++ src/ex02_Java_Basic/Lab016_InterviewQues2.java
@@ -0,0 +1,13 @@
+package ex02_Java_Basic;
+
+public class Lab016_InterviewQues2 {
+    public static void main(String[] args) {
+       // int enum = 10; enum is a keyword
+        int Enum = 10;
+        int a_b = 89;
+        //int Aisha Misba = 89; This is invalid as there cannot be a space between variable
+        int Aisha$Misba =89;
+        int Lab016 = 123;
+
+    }
+}
